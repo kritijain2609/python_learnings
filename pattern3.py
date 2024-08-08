a50f99506eec3486b37ac584550c0e28bc1d5b41
kriti jain <157522704+kritijain2609@users.noreply.github.com>
2024-08-08 14:50:29 +0530

    Create pattern3.py

---
+++ pattern3.py
@@ -0,0 +1,11 @@
+n=5
+for i in range (n):
+    p=1
+    for j in range(i+1):
+        print(p,end=' ')
+        p=p+1    
+    print('')        
+        
+
+                                     
+  
